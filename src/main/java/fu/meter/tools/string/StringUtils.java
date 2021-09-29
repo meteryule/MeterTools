172f833ee912c6e9b1234df67a3ac47f90c3961e
meter <xiangfa.fu@opay-inc.com>
2021-09-29 17:09:51 +0800

    init project

---
+++ src/main/java/fu/meter/tools/string/StringUtils.java
@@ -0,0 +1,12 @@
+package fu.meter.tools.string;
+
+/**
+ * @author meter
+ * @version V1.0
+ * Copyright ©2021
+ * @ClassName StringUtils
+ * @desc 字符串工具
+ * @date 2021/9/29 17:09
+ */
+public class StringUtils {
+}
